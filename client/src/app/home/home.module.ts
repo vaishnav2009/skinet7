0c8dff8c695a1e3bf3e8c3871d7410309a94eba1
vaishnav2009 <vsreekumar4u@gmail.com>
2024-05-26 13:27:30 +0100

    End of Section 12

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomeComponent
--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShopComponent } from './shop.component';
+import { BreadcrumbComponent } from 'xng-breadcrumb';
 
 const routes: Routes = [
   {path: '', component: ShopComponent},
-  {path: ':id', component: ProductDetailsComponent}
+  {path: ':id', component: ProductDetailsComponent, data: {breadcrumb: {alias: 'productDetails'}}}
 ]
 
 @NgModule({
